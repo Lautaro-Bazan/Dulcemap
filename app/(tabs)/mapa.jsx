754709b93b6be2332330f956a66d9e4d5986ad8a
Lean-0n <vallad.leandro@outlook.com>
2025-09-16 19:40:47 -0300

    incluye laravel

--- app/(tabs)/mapa.jsx
+++ app/(tabs)/mapa.jsx
@@ -1,6 +1,19 @@
 import { View, Text, StyleSheet } from "react-native";
-
+import axios from "axios";
+import { useEffect } from "react";
 export default function mapa() {
+  useEffect(() => {
+      axios.get("http://localhost:8000/api/productos") // si usas emulador Android
+      .then(res => {
+        // setProductos(res.data);
+        console.log(res.data);
+        datos = res.data;
+      })
+      .catch(err => {
+        console.error("Error cargando productos:", err);
+      });
+      // .finally(() => setLoading(false));
+  }, []);
   return (
     <View style={styles.container}>
       <Text>mapa</Text>
